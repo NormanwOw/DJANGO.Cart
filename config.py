128b2e0d6a3e8c795884fab5f25684ea30eeb80c
NormanwOw <normjkeee@vk.com>
2024-02-06 20:20:58 +0500

    v0.0.1 -initial commit -added main app -added config

---
+++ config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env-non-dev2')
+
+    DEBUG: int
+
+    SECRET_KEY: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    SMTP_USER: str
+    SMTP_PASSWORD: str
+    SMTP_HOST: str
+    SMTP_PORT: int
+
+
+settings = Settings()
